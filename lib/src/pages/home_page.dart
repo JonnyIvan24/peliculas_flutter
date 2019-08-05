c5f6d2eae5d75791eab7320f670b090fba95f707
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-05 14:13:37 -0500

    widget perzonalizado - Cardswiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 
-import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -31,21 +31,9 @@ class HomePage extends StatelessWidget {
 
   Widget _swiperTargetas() {
 
-    return Container(
-      padding: EdgeInsets.only(top: 10.0),
-      width: double.infinity, // hacemos uso de todo el ancho posible
-      height: 300.0,
-      child: Swiper(
-        layout: SwiperLayout.STACK,
-        itemWidth: 200.0,
-          itemBuilder: (BuildContext context,int index){
-            return new Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,);
-          },
-          itemCount: 3,
-          //pagination: new SwiperPagination(),
-          //control: new SwiperControl(),
-      ),
-    );
+    return CardSwiper(
+      peliculas: [1, 2, 3, 4, 5],
+      );
 
   }
 
---
+++ lib/src/widgets/card_swiper_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class CardSwiper extends StatelessWidget {
+
+  final List<dynamic> peliculas;
+
+  // al utilizar @required indicamos que tenemos que mandar de afuerzas la lista, para poder utilizar esta clase
+  // en este caso el widget perzonalizado
+  CardSwiper({ @required this.peliculas });
+
+  @override
+  Widget build(BuildContext context) {
+
+    // MediaQuery nos da informacion del dispositivo como ancho y alto de la panatalla
+    final _screenSize = MediaQuery.of(context).size;
+
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: _screenSize.width * 0.7,
+        itemHeight: _screenSize.height * 0.5,
+        itemBuilder: (BuildContext context,int index){
+          return ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.network("http://via.placeholder.com/350x150",fit: BoxFit.fill,),
+          );
+        },
+        itemCount: peliculas.length,
+        //pagination: new SwiperPagination(),
+        //control: new SwiperControl(),
+      ),
+    );
+  }
+}
