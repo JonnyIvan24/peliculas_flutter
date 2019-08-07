d76d5351f68e76e556473998f8854327a9d6029c
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-07 15:29:01 -0500

    BuildActions y BuildLeading de SearchDelegate

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -9,25 +9,42 @@ class DataSearch extends SearchDelegate {
   @override
   List<Widget> buildActions(BuildContext context) {
     // Acciones de nuestro navBar
-    return null;
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          // la variable query es el texto que tenemos en el input del search
+          query = '';
+        },
+      )
+    ];
   }
 
   @override
   Widget buildLeading(BuildContext context) {
     // Los iconos a la izquierda de appBar
-    return null;
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        // con la función cerramos la ventana del SearchDelegate, nos pide el contexto y los resultados
+        close(context, null);
+      },
+    );
   }
 
   @override
   Widget buildResults(BuildContext context) {
     // Es a instrucción que crea los resultados a mostrar
-    return null;
+    return Container();
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen al ir escribiendo
-    return null;
+    return Container();
   }
 
 }
