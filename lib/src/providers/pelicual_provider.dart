184c5e0009d71748858583807fb8b417a63f4ee2
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-07 01:57:15 -0500

    servicio de actores

--- lib/src/providers/pelicual_provider.dart
+++ lib/src/providers/pelicual_provider.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 import 'package:peliculas/src/models/pelicula_model.dart';
+import 'package:peliculas/src/models/actores_model.dart';
 
 class PeliculasProvider {
 
@@ -87,6 +88,22 @@ class PeliculasProvider {
     _cargando = false;
 
     return resp;
+
+  }
+
+  Future<List<Actor>> getCast(String peliId) async {
+
+    final url = Uri.https(_url, '3/movie/$peliId/credits', {
+      'api_key': _apyKey,
+      'language': _language
+    });
+
+    final resp = await http.get(url);
+    final decodeData = json.decode(resp.body);
+
+    final cast = new Cast.jsonList(decodeData['cast']);
+
+    return cast.actores;
 
   }
 
